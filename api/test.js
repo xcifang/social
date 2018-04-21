c606e9951a235d8d9b660ba816bce19511207ac0
zhoujie <zhoujie@droi.com>
2018-04-21 19:57:24 +0800

    test

--- api/test.js
+++ api/test.js
@@ -7,8 +7,10 @@ AV.Cloud.define('test', function(request) {
 	focus.set('user_id', 1);
 	focus.set('focus_user_id', 2);
 	focus.save().then(function(focus){
+		console.log("1");
 		return "success";	
 	}, function(error){
+		console.log(error);
 		return error;
 	});
 });
