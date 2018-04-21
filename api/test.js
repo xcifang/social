6cdb9b163ecd6bfda72e76fd850cbd9ca58ee93c
zhoujie <zhoujie@droi.com>
2018-04-21 19:59:34 +0800

    test

--- api/test.js
+++ api/test.js
@@ -4,8 +4,8 @@ AV.Cloud.define('test', function(request) {
 	var UserFocus = AV.Object.extend('user_focus');
 	var focus = new UserFocus();
 	
-	focus.set('user_id', 1);
-	focus.set('focus_user_id', 2);
+	focus.set('user_id', "1");
+	focus.set('focus_user_id', "2");
 	focus.save().then(function(focus){
 		console.log("1");
 		return "success";	
