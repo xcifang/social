2d874a0d9bb1907fc238454e9ed05a4da7095fe3
zhoujie <zhoujie@droi.com>
2018-04-21 19:45:36 +0800

    test

--- api/test.js
+++ api/test.js
@@ -1,5 +1,14 @@
 var AV = require('leanengine');
   
 AV.Cloud.define('test', function(request) {
-  return 'Hello test!';
+	var UserFocus = AV.Object.extend('user_focus');
+	var focus = new UserFocus();
+	
+	focus.set('user_id', 1);
+	focus.set('focus_user_id', 2);
+	tudo.save().then(function(){
+		return "success";	
+	}, function(){
+		return "error";
+	});
 });
